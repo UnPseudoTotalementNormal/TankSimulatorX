37eaebe3f26e29ece459464496dcec862c24e996
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-03-14 21:19:12 +0100

    fallspeed text block on side

--- TankSimulator/Assets/Scripts/GUI/HUD/PlayerWorldCanvas.cs
+++ TankSimulator/Assets/Scripts/GUI/HUD/PlayerWorldCanvas.cs
@@ -15,6 +15,10 @@ public class PlayerWorldCanvas : MonoBehaviour
 
     private void FallSpeedUpdate()
     {
+        Transform textTransform = _fallSpeedText.transform;
+        float maxSidePos = (Camera.main.orthographicSize / 2) / 1.5f;
+        textTransform.localPosition = new Vector3(0, textTransform.localPosition.y, textTransform.localPosition.z);
+        textTransform.position = new Vector3(Mathf.Clamp(textTransform.position.x, -maxSidePos, maxSidePos), textTransform.position.y, textTransform.position.z);
         _fallSpeedText.text = ((int)-_playerRb.velocity.y).ToString() + "<size=-50>km/h";
     }
 }
