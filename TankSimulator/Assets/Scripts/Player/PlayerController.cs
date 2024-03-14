91bfdbaa151806e041bd3e46840fe012ed35ed8d
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-03-14 10:30:38 +0100

    added slow motion & more force when needed

--- TankSimulator/Assets/Scripts/Player/PlayerController.cs
+++ TankSimulator/Assets/Scripts/Player/PlayerController.cs
@@ -8,8 +8,15 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rb;
     [SerializeField] private Transform _canonPivot;
 
+    [SerializeField] private Transform _endCanon;
+
+    [SerializeField] private GameObject _bullet;
+
+    [SerializeField] private GameObject _shootParticle;
+
     private Vector2 _lastJoystickValue;
 
+    [SerializeField] private float _maxFallSpeed = 10;
     [SerializeField] private float _canonForce = 5;
 
 
@@ -27,7 +34,23 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if (_rb.velocity.y < -_maxFallSpeed)
+        {
+            _rb.gravityScale = -0.15f;
+        }
+        else
+        {
+            _rb.gravityScale = 2;
+        }
 
+        if (InputManager.Instance.IsAiming())
+        {
+            Time.timeScale = Mathf.Lerp(Time.timeScale, 0.25f, 4 * Time.timeScale);
+        }
+        else
+        {
+            Time.timeScale = Mathf.Lerp(Time.timeScale, 1f, 4 * Time.timeScale);
+        }
     }
 
     private void UpdateCanonOrientation()
@@ -39,6 +62,23 @@ public class PlayerController : MonoBehaviour
 
     private void ShootCanon()
     {
-        _rb.AddForce(-_lastJoystickValue.normalized * _canonForce, ForceMode2D.Impulse);
+        float addedForce = 0;
+        if (Vector3.Dot(_lastJoystickValue.normalized, _rb.velocity) >= _canonForce/2)
+        {
+            addedForce += _canonForce/2;
+        }
+        _rb.AddForce(-_lastJoystickValue.normalized * (_canonForce + addedForce), ForceMode2D.Impulse);
+
+        if (_bullet)
+        {
+
+        }
+        else Debug.LogWarning("No bullet on playercontroller");
+
+        if (_shootParticle)
+        {
+            Destroy(Instantiate(_shootParticle, _endCanon.position, _canonPivot.rotation), 5);
+        }
+        else Debug.LogWarning("No shootParticle on playercontroller");
     }
 }
